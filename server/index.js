a8b9037753c1eac7237220e499a4373c6672085b
ilya2s <ilyass.elouazzani97@outlook.fr>
2024-01-10 16:50:31 -0500

    cleaning out index.js

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
 const express = require("express");
-
 const app = express();
+const port = 5000;
 
 app.get("/hello", (request, response) => {
     response.status(200).json({
         "message": "hello world!"
     });
 });
-
-const port = 5000;
 
 app.listen(port, () => {
     console.log(`Server is listening on http://localhost:${port}`);
