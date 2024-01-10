ed219da03af21e7a78e9278b738ef5a32ef5dec3
ilya2s <ilyass.elouazzani97@outlook.fr>
2024-01-10 18:46:15 -0500

    backend communication with frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+
+const URL = "/api/tasks";
+
 function App() {
-    return (
+    const [message, setMessage] = useState("");
+
+    useEffect(() => { 
+        const getTasks = async () => {
+            const response = await fetch(URL);
+            const tasks = await response.json();
+
+            setMessage(tasks.message);
+        };
+
+        getTasks();
+    }, []);
+
+    return(
         <main className="container">
             <h1>Obitasks</h1>
+            {message && <p>{message}</p>}
         </main>
     );
 }
