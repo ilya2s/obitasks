644715e05b9ebea8f6b8aaa5c42184f5d818bfed
ilya2s <ilyass.elouazzani97@outlook.fr>
2024-01-11 16:13:02 -0500

    Deployment ready build

--- server/index.js
+++ server/index.js
@@ -4,9 +4,15 @@ const { connectToMongoDB } = require("./database");
 const express = require("express");
 const app = express();
 const router = require("./routes");
+const path = require('path');
 const port = process.env.PORT || 5000;
 
 app.use(express.json());    // to be able to read json object sent to server
+app.use(express.static(path.join(__dirname, 'build')));
+app.get("/", (request, response) => {
+    response.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
 app.use("/api", router);    // prefix endpoints
 
 const startServer = async () => {
